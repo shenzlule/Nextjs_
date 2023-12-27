7a0948ec316df21ed2bff4768522b1c127538a56
Shenzlule <shenzlule65@gmail.com>
2023-12-27 11:05:29 +0300

    commit3

--- src/app/components/Boost.tsx
+++ src/app/components/Boost.tsx
@@ -9,7 +9,7 @@ const Boost = ( ) =>  {
         <div className="flex h-autp py-5 flex-col justify-items-center  items-center ">   
 
           <div className="flex flex-col text-center h-auto mb-8 items-center">
-            <h1 className='text-4xl  m-2'>Looking to boost your <br /> R&D? Let's Talk about it.</h1>
+            <h1 className='text-4xl  m-2'>Looking to boost your <br /> R&D? Let&apos;s Talk about it.</h1>
                 <p className="text-md m-5 gryclr">
                 TLVTech was a refreshing change from other vendors as they were fully in sync with our results and vision for the <br/>
  future.Together we planned,executed and scaled our product.
